00782c906d0ec2f6f28d641b279de86246e6737c
Aliaksei <shebalinas2000@gmail.com>
2023-03-25 20:10:43 +0100

    JS

--- src/index.tsx
+++ src/index.tsx
@@ -7,3 +7,44 @@ ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
+
+
+
+
+
+let a = {
+    name: 'Alex',
+    age: 20,
+    address: {
+        city: 'Warsaw',
+        country: 'Belarus'
+    }
+};
+
+let users = [
+    {
+        name: 'Alex',
+        age: 20,
+        address: {
+            city: 'Warsaw',
+            country: 'Belarus'
+        }
+    },
+{
+    name: 'Al3ex',
+        age: 213,
+    address: {
+    city: 'Warsaw',
+        country: 'Belarus'
+}
+},
+]
+
+let b: Array<number | string>;
+b = [1,1,2,3,4,5];
+b = ['hfffdf', 'dfdfdfd'] ;
+b = [];
+b = [true, false]
+
+
+console.log(users[0].address.city);
