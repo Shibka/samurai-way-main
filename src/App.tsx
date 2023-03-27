04acacb5909a1bbb6f6fc388e20e7ead1454457e
Aliaksei <shebalinas2000@gmail.com>
2023-03-27 15:53:10 +0200

    JS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import "./App.css";
 import Accordion from "./components/Accordion/Accordion";
-function hello() {
-  debugger;
-  alert("Hello Alex");
-}
+import { Rating as R } from "./components/Rating/Rating";
 
 function App() {
   console.log("App rendering");
@@ -12,9 +9,9 @@ function App() {
   return (
     <div>
       <AppTitle />
-      <Rating />
+      <R />
       <Accordion />
-      <Rating />
+      <R />
     </div>
   );
 }
@@ -22,24 +19,6 @@ function App() {
 function AppTitle() {
   console.log("AppTitle rendering");
   return <>This is App component</>;
-}
-
-function Rating() {
-  console.log("Rating rendering");
-  return (
-    <>
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-      <Star />
-    </>
-  );
-}
-
-function Star() {
-  console.log("Star rendering");
-  return <div>star</div>;
 }
 
 export default App;
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export function Rating() {
+  console.log("Rating rendering");
+  return (
+    <>
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+    </>
+  );
+}
+
+function Star() {
+  console.log("Star rendering");
+  return <span>star</span>;
+}
