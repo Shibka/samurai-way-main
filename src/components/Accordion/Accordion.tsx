a910c9e475ebd62a73f34b15b6b1cff2993e2c3f
Aliaksei <shebalinas2000@gmail.com>
2023-04-02 12:40:33 +0200

    JS

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,26 +1,47 @@
 import React from "react";
-function Accordion() {
-  console.log("Accordion rendering");
+
+type AccordionPropsType = {
+  titleValue: string;
+  collapsed: boolean;
+};
+
+function Accordion(props: AccordionPropsType) {
+  if (props.collapsed === true) {
+    return (
+      <>
+        <AccordionTitle title={props.titleValue} />
+        <AccordionBody />
+      </>
+    );
+  }
   return (
     <>
-      <AccordionTitle />
-      <AccordionBody />
+      <AccordionTitle title={props.titleValue} />
     </>
   );
 }
-function AccordionTitle() {
-  console.log("AccordionTitle rendering");
-  return <h3>Menu</h3>;
+
+type AccordionTitlePropsType = {
+  title: string;
+};
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+  return (
+    <>
+      <h3>{props.title}</h3>
+    </>
+  );
 }
 
 function AccordionBody() {
-  console.log("AccordionBody rendering");
   return (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
   );
 }
 
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export function Rating(props: any) {
+type RatingPropsType = {
+  value: number;
+};
+
+export function Rating(props: RatingPropsType) {
   console.log("Rating rendering");
 
   if (props.value === 1) {
@@ -69,7 +73,11 @@ export function Rating(props: any) {
   );
 }
 
-function Star(props: any) {
+type StarPropsType = {
+  selected: boolean;
+};
+
+function Star(props: StarPropsType) {
   console.log("Star rendering");
   if (props.selected === true) {
     return (
