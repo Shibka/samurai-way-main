8c0e5aed689dba74ec57a2d4a9837d240abe7d01
Aliaksei <shebalinas2000@gmail.com>
2023-09-01 18:55:10 +0200

    Changed

--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -1,52 +1,58 @@
-import React, { useState } from "react";
+import React, {useReducer} from "react";
+import {reducer, TOGGLE_CONSTANT} from "./Reducer";
 
 type AccordionPropsType = {
-  titleValue: string;
-  // collapsed: boolean;
+    titleValue: string;
 };
 
-function UncontrolledAccordion(props: AccordionPropsType) {
-  let [collapsed, setCollapsed] = useState(true);
 
-  return (
-    <div>
-      <AccordionTitle
-        title={props.titleValue}
-        onClick={() => setCollapsed(!collapsed)}
-      />
-      {collapsed && <AccordionBody />}
-    </div>
-  );
+function UncontrolledAccordion(props: AccordionPropsType) {
+    // let [collapsed, setCollapsed] = useState(false);
+    let [collapsed, dispatch] = useReducer(reducer, false);
+
+    return (
+        <div>
+            <AccordionTitle
+                title={props.titleValue}
+                // onClick={() => {setCollapsed(!collapsed)}}
+                onClick={() => {
+                    dispatch({type: TOGGLE_CONSTANT})
+                }}
+            />
+            {!collapsed && <AccordionBody/>}
+        </div>
+    );
 }
+
 type AccordionTitlePropsType = {
-  title: string;
-  onClick: () => void;
+    title: string;
+    onClick: () => void;
 };
 
 function AccordionTitle(props: AccordionTitlePropsType) {
-  return (
-    <>
-      <h3
-        onClick={() => {
-          props.onClick();
-        }}
-      >
-        {props.title}
-      </h3>
-    </>
-  );
+    return (
+        <>
+            <h3
+                onClick={() => {
+                    props.onClick();
+                }}
+            >
+                {props.title}
+            </h3>
+        </>
+    );
 }
 
 function AccordionBody() {
-  return (
-    <div>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
 }
 
 export default UncontrolledAccordion;
