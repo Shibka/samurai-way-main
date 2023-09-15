ad125f8de3752e5741dd9a2a520a99437793cdb5
Aliaksei <shebalinas2000@gmail.com>
2023-09-15 16:12:32 +0200

    Changed

---
+++ src/stories/SelectExample.stories.tsx
@@ -0,0 +1,42 @@
+import React, {ChangeEvent, useState} from "react";
+import type {Meta, StoryObj} from "@storybook/react";
+import { Select } from "../components/Select/Select";
+
+
+
+const meta: Meta<typeof Select> = {
+    title: "Select",
+    component: Select,
+};
+
+export default meta;
+
+type Select = StoryObj<typeof meta>;
+
+export const Collapsed: Select = {
+    args: {
+        onChange: () => {
+        },
+        items: [{title: 'Alex', value: 1}, {title: 'Leha', value: 2}, {title: 'Gleb', value: 3}],
+    },
+};
+export const ChangingSelect: Select = {
+    render: (args) => {
+        // eslint-disable-next-line
+        const [value, setValue] = useState(1);
+        // eslint-disable-next-line
+let [items, setItems]= useState([
+    {title: 'Alex', value: 1},
+    {title: 'Leha', value: 2},
+    {title: 'Gleb', value: 3}
+])
+        return (
+            <Select
+                {...args}
+                onChange={setValue}
+                value={value}
+                items={items}
+            />
+        );
+    },
+};
